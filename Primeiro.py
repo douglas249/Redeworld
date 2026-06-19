e8ab6e9958b16fd5601b1eb859ac4fc14ecc1e6e
douglas249 <rodriguescristiandouglas@gmail.com>
2026-06-19 19:39:56 -0300

    terminei de fazer o calculo de distancia entre os computadores e fiz p calculo de quantida de caixas de cabo de rede serão necessarios

--- Primeiro.py
+++ Primeiro.py
@@ -1,3 +1,4 @@
+import math
 #pegando informaçoes#
 print ('Tem que ter a requisição de equipamentos, por favor digite abaixo os equipamentos:')
 
@@ -15,12 +16,16 @@ cabo3 = servidordearq * 2
 cabo4 = roteadores * 2 
 #Resposta:#
 print ('A quantidade de computadores é {} e a quantidade de conectores para conexão é {}'.format(computadores,cabo1))
+
 print ('A quantidade de impressoras que serão utilizados é {} e a qauntidade de conectores necessários é {}'.format(impressoras, cabo2))
+
 print ('A quantidade de servidores que serão utilizados é {} e a quantidade de conectores necessários é {} '.format(servidordearq, cabo3))
+
 print ('A quantidade de roteadores utilizados é {} e a quantidade de conectores necessários {}'.format(roteadores, cabo4))
 #Calculando o tamanho da area e distancia dos equipamentos#
-area = int(input('Qual é a qauntidade de metros quadrados do seu projeto?'))
-distancia = int(input('Qual vai ser a distancia entre os entre os equipamentos?'))
+area = int(input('Qual é a quantidade de metros quadrados do seu projeto?'))
+
+distancia = int(input('Qual vai ser a distancia entre os equipamentos?'))
 total = distancia * computadores
 total1 = total + area
 #Dando resposta de acordo com o tamanho da distancie entre os computadores#
@@ -32,3 +37,25 @@ elif distancia >= 21 and distancia <= 30:
     print ('A area total dos computadores é {}'.format(total1))
 elif distancia >= 31 and distancia <= 40:
     print ('A area total dos computadores é {}'.format(total1))             
+elif distancia >= 41 and distancia <= 50:
+    print ('A area total dos computadores é {}'.format(total1))
+elif distancia >= 51 and distancia <= 60:
+    print ('A area total dos computadores é {}'.format(total1))
+elif distancia >= 61 and distancia <= 70:
+    print ('A area total dos computadores é {}'.format(total1))             
+elif distancia >= 71 and distancia <= 80:
+    print ('A area total dos computadores é {}'.format(total1))
+elif distancia >= 81 and distancia <= 90:
+    print ('A area total dos computadores é {}'.format(total1))
+elif distancia >= 91 and distancia <= 100:
+    print ('A area total dos computadores é {}'.format(total1))   
+#Fazendo o calculo de quantas caixas de cabo de rede serão necessárias para o projeto#
+metros = total1
+caixas = metros / 305
+#resposta#
+print ('A área total do seu projeto é de {}m² e irá precisar de {} caixas de cabo de rede'.format(total1,math.ceil(caixas)))
+
+
+
+
+ 
